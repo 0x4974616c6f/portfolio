71885bff8ba1eec18070ddabe0052860786c9289
ItaloCobains <italobrandao.contato@gmail.com>
2024-01-23 21:37:46 -0300

    add new jobs

--- Experience/Experience.js
+++ Experience/Experience.js
@@ -14,54 +14,54 @@ import World from "./World/World.js";
 import Controls from "./World/Controls.js";
 
 export default class Experience {
-    static instance;
-    constructor(canvas) {
-        if (Experience.instance) {
-            return Experience.instance;
-        }
-        Experience.instance = this;
-        this.canvas = canvas;
-        this.scene = new THREE.Scene();
-        this.time = new Time();
-        this.sizes = new Sizes();
-        this.camera = new Camera();
-        this.renderer = new Renderer();
-        this.resources = new Resources(assets);
-        this.theme = new Theme();
-        this.world = new World();
-        this.preloader = new Preloader();
+  static instance;
+  constructor(canvas) {
+    if (Experience.instance) {
+      return Experience.instance;
+    }
+    Experience.instance = this;
+    this.canvas = canvas;
+    this.scene = new THREE.Scene();
+    this.time = new Time();
+    this.sizes = new Sizes();
+    this.camera = new Camera();
+    this.renderer = new Renderer();
+    this.resources = new Resources(assets);
+    this.theme = new Theme();
+    this.world = new World();
+    this.preloader = new Preloader();
 
-        this.preloader.on("enablecontrols", () => {
-            this.controls = new Controls();
-        });
+    this.preloader.on("enablecontrols", () => {
+      this.controls = new Controls();
+    });
 
-        this.sizes.on("resize", () => {
-            this.resize();
-        });
-        this.time.on("update", () => {
-            this.update();
-        });
+    this.sizes.on("resize", () => {
+      this.resize();
+    });
+    this.time.on("update", () => {
+      this.update();
+    });
 
-        window.addEventListener('load', function() {
-        
-        var audio = new Audio('../public/textures/music.mp3')
-        audio.play()
+    window.addEventListener('load', function() {
+
+      var audio = new Audio('../public/textures/music.mp3')
+      audio.play()
     })
-    }
+  }
 
-    resize() {
-        this.camera.resize();
-        this.world.resize();
-        this.renderer.resize();
-    }
+  resize() {
+    this.camera.resize();
+    this.world.resize();
+    this.renderer.resize();
+  }
 
-    update() {
-        this.preloader.update();
-        this.camera.update();
-        this.world.update();
-        this.renderer.update();
-        if (this.controls) {
-            this.controls.update();
-        }
+  update() {
+    this.preloader.update();
+    this.camera.update();
+    this.world.update();
+    this.renderer.update();
+    if (this.controls) {
+      this.controls.update();
     }
+  }
 }
